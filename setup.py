860dbbe4afa47b562844fb6ccd14d937b82833b8
abbfaria <vonap at proton dot me>
2025-12-23 16:45:52 +0200

    Add basic Python project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ci-cd-learning",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    test_suite="tests",
+)
